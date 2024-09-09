a33167cf82741f48bb6ef3c724883ecc82d86362
Роза Жайсанова <fonelor8@gmail.com>
2024-09-09 09:26:24 +0300

    added Badge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { HeaderStyled, Logo } from "./styles";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-export const Header = () => (
-  <HeaderStyled>
-    <Logo to="/">Audio Store</Logo>
-    <Link to="/cart">
-      <ShoppingCartIcon />
-    </Link>
-  </HeaderStyled>
-);
+import { Badge } from "@mui/material";
+import { useCart } from "../../contexts/CartContext";
+export const Header = () => {
+  const { cart } = useCart();
+
+  return (
+    <HeaderStyled>
+      <Logo to="/">Audio Store</Logo>
+      <Link to="/cart">
+        <Badge badgeContent={cart.itemsCount} color="primary">
+          <ShoppingCartIcon />
+        </Badge>
+      </Link>
+    </HeaderStyled>
+  );
+};
