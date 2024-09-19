43902cbb1182cf3b740d16208b7349b6a1e95368
Роза Жайсанова <fonelor8@gmail.com>
2024-09-19 14:36:44 +0300

    added footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useLanguage } from "../../contexts";
 import { useTranslation } from "react-i18next";
+import { ButtonStyled } from "../CartProduct/styles";
 const FooterContainer = styled.footer`
   box-shadow: 0px 0px 20px 0px #0000001a;
   width: 1110px;
@@ -35,32 +36,59 @@ const PhoneLink = styled.a`
     text-decoration: underline;
   }
 `;
+const FooterContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.div`
+  margin: 0 20px; // Отступы между колонками
+`;
+const SiteName = styled.img`
+  padding-left: 29px;
+  padding-top: 29px;
+`;
 const Footer = () => {
   const { t } = useTranslation();
   const { setLanguage } = useLanguage();
 
-  const handleLanguageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setLanguage(event.target.value);
-  };
-
   return (
     <FooterContainer>
-      <LanguageSelector onChange={handleLanguageChange}>
-        <option value="en">English</option>
-        <option value="ru">Русский</option>
-        <option value="es">Español</option>
-      </LanguageSelector>
-      <h1>{t("termsOfService")}</h1>
+      <SiteName src="./assets/QPICK.png" alt="qpick" />
+
+      <FooterContent>
+        <Column></Column>
+        <Column>
+          <h2>{t("termsOfService")}</h2>
+          <div>
+            <img src="./assets/globus.png" alt="Иконка глобуса" />
+            <ButtonStyled type="button" onClick={() => setLanguage("ru")}>
+              Рус
+            </ButtonStyled>
+            <ButtonStyled type="button" onClick={() => setLanguage("eng")}>
+              Eng
+            </ButtonStyled>
+          </div>
+        </Column>
+      </FooterContent>
+      <SocialLink
+        href="https://vk.com/sqrtx"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="./assets/VK.svg" alt="VK" />
+      </SocialLink>
       <SocialLink
         href="https://t.me/fonelor"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Telegram
+        <img src="./assets/Telegram.svg" alt="Telegram" />
       </SocialLink>
-      <PhoneLink href="tel:+79805304119">+7 (980) 530-4119</PhoneLink>
+      <PhoneLink href="tel:+79805304119">
+        <img src="./assets/Whatsapp.svg" alt="Whatsapp" />
+      </PhoneLink>
     </FooterContainer>
   );
 };
