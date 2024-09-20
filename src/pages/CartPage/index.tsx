fcb9c61a730a7bf38fe0e96eb0364063ea5f51e1
Роза Жайсанова <fonelor8@gmail.com>
2024-09-20 19:30:58 +0300

    fixed totalPrice position

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -3,11 +3,11 @@ import { useSelector } from "react-redux";
 import { selectCartItems } from "../../store/slices/cartSlice";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import PaymentModal from "../../components/PaymentModal";
 import { ContainerStyled } from "../../components/ProductList/styles";
 import { CartProductStyled } from "../../components/CartProduct/styles";
-import { ColumnStyled, DivStyled, TotalStyled } from "./styles";
+import { ColumnStyled, PaymentButtonStyled, TotalStyled } from "./styles";
 function CartPage() {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
@@ -22,54 +22,68 @@ function CartPage() {
     <ContainerStyled>
       <main className="main">
         <h1>Корзина</h1>
-        <DivStyled>
-          {cartItems.length === 0 ? (
-            <p>Корзина пуста</p>
-          ) : (
-            cartItems.map((item) => (
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  marginTop: "20px",
-                }}
-              >
-                <CartProductStyled>
-                  <CartProduct
-                    key={item.title}
-                    title={item.title}
-                    img={item.img}
-                    price={item.price}
-                    quantity={item.quantity}
-                  />
-                </CartProductStyled>
-              </div>
-            ))
-          )}
-          <div
-            style={{
+        <Box
+          sx={{
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            {cartItems.length === 0 ? (
+              <p>Корзина пуста</p>
+            ) : (
+              cartItems.map((item) => (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginTop: "20px",
+                  }}
+                >
+                  <CartProductStyled>
+                    <CartProduct
+                      key={item.title}
+                      title={item.title}
+                      img={item.img}
+                      price={item.price}
+                      quantity={item.quantity}
+                    />
+                  </CartProductStyled>
+                </div>
+              ))
+            )}
+          </Box>
+
+          <Box
+            sx={{
+              textAlign: "right",
               display: "flex",
-              justifyContent: "flex-end",
-              marginTop: "20px",
+              flexDirection: "column",
+              alignItems: "flex-end",
             }}
           >
-            {totalPrice !== 0 ? (
-              <TotalStyled>
-                ₽ {totalPrice}
-                <Button onClick={handleOpen}>Переход к оформлению</Button>
-              </TotalStyled>
-            ) : (
-              <span>emptyCart</span>
-            )}
-          </div>
-          <ColumnStyled>ff</ColumnStyled>
-          <ColumnStyled>ff</ColumnStyled>
-        </DivStyled>
-        {totalPrice !== 0 ? (
-          <TotalStyled>₽ {totalPrice}</TotalStyled>
-        ) : (
-          <span>emptyCart</span>
-        )}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginTop: "20px",
+              }}
+            >
+              {totalPrice !== 0 ? (
+                <TotalStyled>
+                  ₽ {totalPrice}
+                  <PaymentButtonStyled onClick={handleOpen}>
+                    Переход к оформлению
+                  </PaymentButtonStyled>
+                </TotalStyled>
+              ) : (
+                <span>emptyCart</span>
+              )}
+            </div>
+          </Box>
+        </Box>
 
         <PaymentModal open={open} handleClose={handleClose} />
       </main>
--- src/pages/CartPage/styles.ts
+++ src/pages/CartPage/styles.ts
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+import { ButtonStyled } from "./../../components/CartProduct/styles";
 import styled from "styled-components";
 export const CardStyled = styled.div`
   box-shadow: 0px 0px 20px 0px #0000001a;
@@ -28,7 +30,14 @@ export const TotalStyled = styled.div`
   border-radius: 30px;
   opacity: 0px;
 `;
-export const DivStyled = styled.div`
-  display: flex;
-  flex-direction: column;
+export const PaymentButtonStyled = styled(Button)`
+  box-shadow: 0px 0px 20px 0px #00000040;
+  background: #000000;
+  width: 348.98px;
+  height: 65px;
+  top: 188px;
+  left: 926.02px;
+  gap: 0px;
+  border-radius: 20px;
+  opacity: 0px;
 `;
